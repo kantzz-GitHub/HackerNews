1e4f5ce99926cd3b818f0e2dfedd79060dfabe30
kantzz <codingblyatt@gmail.com>
2021-10-14 15:19:56 +0500

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by admin on 2021-10-14.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
